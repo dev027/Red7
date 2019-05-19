7178587a4744f544024941258b15cd247b7ba784
Steve Wright <stevewrightuk@gmail.com>
2019-05-19 09:27:19 +0100

    Add project files.

---
+++ Red7.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Red7.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
